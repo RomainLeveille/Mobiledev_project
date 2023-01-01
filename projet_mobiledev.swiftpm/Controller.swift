68300b36ddc8ab374a24bcb644b87ee0d8974b3c
RomainLeveille <romain.leveille-nizerolle@efrei.net>
2023-01-01 20:02:40 +0000

    api!first

---
+++ projet_mobiledev.swiftpm/Controller.swift
@@ -0,0 +1,28 @@
+// Controller
+/*
+ let requestFactory = RequestFactory()
+
+requestFactory.getFurnitureList { (errorHandle, furnitures) in
+    if let _ = errorHandle.errorType, let errorMessage =
+        errorHandle.errorMessage {
+        print(errorMessage)
+    }
+    else if let list = furnitures, let furniture = list.last {
+        print(furniture.id)
+    }
+    else {
+        print("Houston we got a problem")
+    }
+}
+requestFactory.deleteFurniture(with: "rec00x6nIiwAmQjhB") { (errorHandle, response) in
+    if let _ = errorHandle.errorType, let errorMessage =
+        errorHandle.errorMessage {
+        print(errorMessage)
+    }
+    else if let reponse = response {
+        print(reponse.deleted)
+    }
+    else {
+        print("Houston we got a problem")
+    }
+}
